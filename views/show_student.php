18102ca004563e7ce6b1fec0fa5716377e966603
siemdicou <siemdicou@hotmail.com>
2016-01-18 11:37:54 +0100

    php done

--- views/show_student.php
+++ views/show_student.php
@@ -1,12 +1,9 @@
 <?php
+
+	$students = array();
 	while ($student = $result->fetch_assoc()) {
-		echo json_encode($student['Firstname']);
-		echo json_encode($student['Lastname']);
-		echo json_encode($student['Class']);
-		echo json_encode($student['Age']);
-		echo json_encode($student['Height (cm)']);
-		echo json_encode($student['Gender']);
-		echo json_encode($student['Eye color']);
-		echo "<br>";
-	}
+		$students[] = $student;
+		}
+
+	echo json_encode($students);
 ?>
