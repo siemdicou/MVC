de9de5c370afde1e5646dde0c4ac4675e40ad2d0
ivorkill <davevanrossum@gmail.com>
2016-01-18 11:14:40 +0100

    encoded in json

--- views/show_student.php
+++ views/show_student.php
@@ -1,12 +1,12 @@
 <?php
 	while ($student = $result->fetch_assoc()) {
-		echo $student['Firstname'];
-		echo $student['Lastname'];
-		echo $student['Class'];
-		echo $student['Age'];
-		echo $student['Height (cm)'];
-		echo $student['Gender'];
-		echo $student['Eye color'];
+		echo json_encode($student['Firstname']);
+		echo json_encode($student['Lastname']);
+		echo json_encode($student['Class']);
+		echo json_encode($student['Age']);
+		echo json_encode($student['Height (cm)']);
+		echo json_encode($student['Gender']);
+		echo json_encode($student['Eye color']);
 		echo "<br>";
 	}
 ?>
